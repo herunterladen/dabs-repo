1e8798b0b1326edd18b43c18ac26900105fa2127
herunterladen <57572083+herunterladen@users.noreply.github.com>
2021-07-03 13:20:01 +0200

    Create project.ybs.py

---
+++ project.ybs.py
@@ -0,0 +1,14 @@
+class Project:
+
+    def __file(self,_filename):
+        with open(_filename,"r") as stream:
+            self.file = yaml.safe_load(stream)["project"]
+
+    def __init__(self,_filename):
+
+        self.__file(_filename)
+        self.name        = self.file["name"]
+        self.version     = self.file["version"]
+        self.description = self.file["description"]
+        self.languages   = self.file["languages"]
+        self.url         = self.file["url"]
