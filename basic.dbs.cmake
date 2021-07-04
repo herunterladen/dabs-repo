0a150712479279224b5e643aa4383edea5f5f7f9
herunterladen <57572083+herunterladen@users.noreply.github.com>
2021-07-04 12:54:37 +0200

    Update basic.dbs.cmake

--- basic.dbs.cmake
+++ basic.dbs.cmake
@@ -6,7 +6,4 @@ project("{{Project.name}}"
 	HOMEPAGE_URL "{{Project.url}}"
 	LANGUAGES {{Project.languages}}
 	)
-add_executable(${PROJECT_NAME} ../main.cpp)
-find_package(HDF5 COMPONENTS C CXX)
-set(HDF5_USE_STATIC_LIBRARIES ON)
-target_link_libraries(${PROJECT_NAME} PUBLIC  ${HDF5_CXX_LIBRARIES} ${HDF5_LIBRARIES})
+add_executable(${PROJECT_NAME} main.cpp)
